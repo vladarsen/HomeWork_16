89bf000ae2c2e5a1439d3337613f3b3321dadb0b
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-05-01 17:49:58 +0300

    Task_1

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -4,6 +4,16 @@ package com.vladarsenjtev;
 public class Main {
 
     public static void main(String[] args) {
+        SinglyLinkedList<Integer> digits = new SinglyLinkedList<>();
+        digits.add(1);
+        digits.add(5);
+        digits.add(8);
 
+
+        CustomIterator<Integer> inter = digits.iterator();
+
+        while (inter.hasNext()) {
+            System.out.println(inter.next());
+        }
     }
 }
